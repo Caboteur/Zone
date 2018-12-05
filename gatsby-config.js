7ce906dd460b39ef72f0d654b5df972739bddd02
Caboteur <caboteur.adrienblanchot@gmail.com>
2018-12-05 14:06:41 +0100

    retry build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
+
+
+// Wrap the require in check for window
+if (typeof window !== `undefined`) {
+  const module = require("module")
+}
+
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
@@ -25,6 +33,13 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/zoneLogo.svg', // This path is relative to the root of the site.
       },
+      "scripts": {
+    "deploy": "gatsby build --prefix-paths && gh-pages -d public",
+    "build": "gatsby build",
+    "develop": "gatsby develop",
+    "format": "prettier --write 'src/**/*.js'",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
