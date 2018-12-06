f165ce2a531243fcd60c1f3032ccbf1a4811784a
Caboteur <caboteur.adrienblanchot@gmail.com>
2018-12-06 14:21:18 +0100

    index resolve

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import SignIn from '../images/SignIN.svg'
 import SignUp from '../images/SignOUT.svg'
 import  MainmobileOrdi from '../components/MainmobileOrdi.js'
 import  MainpageMobile from '../components/MainpageMobile.js'
-import firebase from '../components/config.js'
 import { navigate } from "gatsby"
 
 
@@ -16,26 +15,10 @@ export default class Indexpage extends React.Component {
   render() {
 
 
-     const handleMobile =  ()=>{
-
-    if( navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)
-  ){
-    return(<MainpageMobile/>)
-  }
-    else{
-    return(<MainmobileOrdi/>)
-    }
-
-    }
     return (
 
       <div className="firstPage">
-           {handleMobile()}
+           <MainpageMobile/>
       </div>
 
           )
